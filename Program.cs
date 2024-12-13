f06a52423f63e5d654ddc3f74fd4ea0c7e40e75f
Oli Lane <olil4ne@gmail.com>
2024-12-13 12:46:01 +0000

    Basic energy usage download

--- Program.cs
+++ Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using ImpSoft.OctopusEnergy.Api;
+using Microsoft.Extensions.Configuration;
+using System.Net.Http.Headers;
+using System.Text;
+// Get your api key, maybe from user secrets
+
+var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
+            
+var secretProvider = config.Providers.First();
+
+secretProvider.TryGet("octopus_api_key", out var apiKey);
+secretProvider.TryGet("electricity_mpan", out var electricityMPAN);
+secretProvider.TryGet("electricity_serial", out var electricitySerial);
+secretProvider.TryGet("gas_mprn", out var gasMPRN);
+secretProvider.TryGet("gas_serial", out var gasSerial);
+
+using var httpClient = new HttpClient();
+httpClient.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", Convert.ToBase64String(Encoding.UTF8.GetBytes(apiKey + ":")));
+
+// Create the api wrapper
+var octopusClient = new OctopusEnergyClient(httpClient);
+
+var from = new DateTimeOffset(2024, 12, 01, 00, 00, 00, TimeSpan.FromHours(1));
+var to = new DateTimeOffset(2024, 12, 13, 23, 59, 00, TimeSpan.FromHours(1));
+
+var consumption = await octopusClient.GetElectricityConsumptionAsync(electricityMPAN, electricitySerial, from, to, Interval.Day);
+consumption.ToList()
+.ForEach(c => Console.WriteLine(
+    $"[{c.Start.ToLocalTime()}-{c.End.ToLocalTime()}), {c.Quantity:00.00}"));
+
+// For SMETS1 this is kwh equivalent, for SMETS2 it is in m^3
+var gasConsumption = await octopusClient.GetGasConsumptionAsync(gasMPRN, gasSerial, from, to, Interval.Day);
+gasConsumption.ToList()
+.ForEach(c => Console.WriteLine(
+    $"[{c.Start.ToLocalTime()}-{c.End.ToLocalTime()}), {c.Quantity:00.00}"));
+
--- README.md
+++ README.md
@@ -1,1 +1,19 @@
 # energy-graphs
+
+To set your secret values:
+
+```
+dotnet user-secrets set octopus_api_key <octopus api key>
+dotnet user-secrets set electricity_mpan <electric meter MPAN>
+dotnet user-secrets set electricity_serial <electric meter serial number>
+dotnet user-secrets set gas_mprn <gas meter MPRN>
+dotnet user-secrets set gas_serial <gas meter serial number>
+```
+
+Get these from https://octopus.energy/dashboard/new/accounts/personal-details/api-access
+
+Note that this saves the secrets in a way that won't get included in this repository but they are not encryped and will remain on your disk and in your terminal history.
+
+## Notes
+Gas units are assumed to be m^3 but this is only true on the API if you have a SMETS2 meter. If you have a SMETS1 meter all gas values will be in kWh.
+
