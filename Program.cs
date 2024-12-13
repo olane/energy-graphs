c9fa998854a49aafc4b77871340f1b3598de5ad9
Oli Lane <olil4ne@gmail.com>
2024-12-13 13:36:50 +0000

    Draw graphs from usage

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿using ImpSoft.OctopusEnergy.Api;
 using Microsoft.Extensions.Configuration;
 var config = new ConfigurationBuilder().AddUserSecrets<Program>().Build();
-            
+
 var secretProvider = config.Providers.First();
 
 secretProvider.TryGet("octopus_api_key", out var apiKey);
@@ -17,17 +17,39 @@ httpClient.SetAuthenticationHeaderFromApiKey(apiKey);
 // Create the api wrapper
 var octopusClient = new OctopusEnergyClient(httpClient);
 
-var from = new DateTimeOffset(2024, 12, 01, 00, 00, 00, TimeSpan.FromHours(1));
-var to = new DateTimeOffset(2024, 12, 13, 23, 59, 00, TimeSpan.FromHours(1));
+var from = new DateTimeOffset(2023, 8, 01, 00, 00, 00, TimeSpan.FromHours(0));
+var to = new DateTimeOffset(2024, 12, 13, 23, 59, 00, TimeSpan.FromHours(0));
 
-var consumption = await octopusClient.GetElectricityConsumptionAsync(electricityMPAN, electricitySerial, from, to, Interval.Day);
-consumption.ToList()
-.ForEach(c => Console.WriteLine(
-    $"[{c.Start.ToLocalTime()}-{c.End.ToLocalTime()}), {c.Quantity:00.00}"));
+var electricConsumption = (await octopusClient.GetElectricityConsumptionAsync(electricityMPAN, electricitySerial, from, to, Interval.Day)).ToList();
 
 // For SMETS1 this is kwh equivalent, for SMETS2 it is in m^3
-var gasConsumption = await octopusClient.GetGasConsumptionAsync(gasMPRN, gasSerial, from, to, Interval.Day);
-gasConsumption.ToList()
-.ForEach(c => Console.WriteLine(
-    $"[{c.Start.ToLocalTime()}-{c.End.ToLocalTime()}), {c.Quantity:00.00}"));
+var gasConsumption = (await octopusClient.GetGasConsumptionAsync(gasMPRN, gasSerial, from, to, Interval.Day)).ToList();
 
+DrawGasUsage(gasConsumption);
+DrawElectricityUsage(electricConsumption);
+
+void DrawGasUsage(List<Consumption> electricConsumption)
+{
+    ScottPlot.Plot gasPlot = new();
+    gasPlot.Axes.DateTimeTicksBottom();
+    gasPlot.Add.Scatter(gasConsumption.Select(x => x.Start.ToLocalTime().DateTime).ToArray(), gasConsumption.Select(x => x.Quantity).ToArray());
+
+    gasPlot.XLabel("Date");
+    gasPlot.YLabel("Consumption (m^3)");
+    gasPlot.Title("Gas usage");
+    gasPlot.Axes.SetLimitsY(0, 12.5);
+    gasPlot.SavePng("output/gas-usage.png", 1000, 800);
+}
+
+void DrawElectricityUsage(List<Consumption> electricConsumption)
+{
+    ScottPlot.Plot leccyPlot = new();
+    leccyPlot.Axes.DateTimeTicksBottom();
+    leccyPlot.Add.Scatter(electricConsumption.Select(x => x.Start.ToLocalTime().DateTime).ToArray(), electricConsumption.Select(x => x.Quantity).ToArray());
+
+    leccyPlot.XLabel("Date");
+    leccyPlot.YLabel("Consumption (kWh)");
+    leccyPlot.Title("Electricity usage");
+    leccyPlot.Axes.SetLimitsY(0, 25);
+    leccyPlot.SavePng("output/electric-usage.png", 1000, 800);
+}
--- README.md
+++ README.md
@@ -14,6 +14,16 @@ Get these from https://octopus.energy/dashboard/new/accounts/personal-details/ap
 
 Note that this saves the secrets in a way that won't get included in this repository but they are not encryped and will remain on your disk and in your terminal history.
 
+Before running create your output folder:
+```
+mkdir output
+```
+
+To run:
+```
+dotnet run
+```
+
 ## Notes
 Gas units are assumed to be m^3 but this is only true on the API if you have a SMETS2 meter. If you have a SMETS1 meter all gas values will be in kWh.
 
